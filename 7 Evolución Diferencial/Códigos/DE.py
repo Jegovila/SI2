252e199aaf7d2de268d8240032d4750520053144
Dr. Javier Gómez <115826517+Jegovila@users.noreply.github.com>
2023-08-16 12:50:13 -0600

    Add files via upload

---
+++ 7 Evolución Diferencial/Códigos/DE.py
@@ -0,0 +1,73 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from Plot_Surf import plot_surf
+from Plot_Contour import plot_contour
+
+f = lambda x, y: (x-2)**2 + (y-2)**2
+#f = lambda x, y: -20 * np.exp(-0.2 * np.sqrt(0.5*(x**2 + y**2))) - np.exp(0.5*(np.cos(2*np.pi*x)+np.cos(2*np.pi*y))) + 20 + np.exp(1)
+#f = lambda x, y: -((1+np.cos(12*np.sqrt(x**2+y**2))) / (0.5*(x**2+y**2)+2))
+#f = lambda x, y: 10*2 + x**2 + y**2 - 10*np.cos(2*np.pi*x) - 10*np.cos(2*np.pi*y)
+#f = lambda x, y: ((x**2/4000)+(y**2/4000))-(np.cos(x)*np.cos(y/np.sqrt(2)))+1
+
+xl = np.array([-5, -5])
+xu = np.array([5, 5])
+
+G = 150
+N = 50
+D = 2
+
+F = 0.6
+CR = 0.9
+
+x = np.zeros((D, N))
+fitness = np.zeros(N)
+
+for i in range(N):
+    x[:, i] = xl + (xu - xl) * np.random.rand(D)
+    fitness[i] = f(x[0, i], x[1, i])
+
+fx_plot = np.zeros(G)
+
+for n in range(G):
+    plot_contour(f, x, xl, xu)
+
+    for i in range(N):
+        # Mutación
+        r1 = i
+        while r1 == i:
+            r1 = np.random.randint(N)
+
+        r2 = r1
+        while r2 == r1 or r2 == i:
+            r2 = np.random.randint(N)
+
+        r3 = r2
+        while r3 == r2 or r3 == r1 or r3 == i:
+            r3 = np.random.randint(N)
+
+        v = x[:, r1] + F * (x[:, r2] - x[:, r3])
+
+        # Recombinación
+        u = np.zeros(D)
+
+        for j in range(D):
+            r = np.random.rand()
+
+            if r <= CR:
+                u[j] = v[j]
+            else:
+                u[j] = x[j, i]
+
+        # Selección
+        fitness_u = f(u[0], u[1])
+
+        if fitness_u < fitness[i]:
+            x[:,i] = u
+            fitness[i] = fitness_u
+
+    fx_plot[n] = np.min(fitness)
+
+igb = np.argmin(fitness)
+
+print("Mínimo global en x=", x[0, igb], " y=", x[1, igb], " f(x,y)=", f(x[0, igb], x[1, igb]))
+plot_surf(f, x, xl, xu, igb)
---
+++ 7 Evolución Diferencial/Códigos/DE_CurrentToRand_1_exp.m
@@ -0,0 +1,90 @@
+clear all
+% close all
+clc
+
+f = @(x,y) (x-2).^2 + (y-2).^2; % Sphere
+% f = @(x,y) -20*exp(-0.2*sqrt(0.5*(x.^2 + y.^2))) - exp(0.5*(cos(2*pi*x)+cos(2*pi*y)))+20+exp(1); % Ackley
+% f = @(x,y) -((1+cos(12*sqrt(x.^2+y.^2)))./(0.5*(x.^2+y.^2)+2)); % Drop-Wave Function
+% f = @(x,y) 10*2 + x.^2 + y.^2 - 10*cos(2*pi*x) - 10*cos(2*pi*y); % Rastrigin
+% f = @(x,y) ((x.^2/4000)+(y.^2/4000))-(cos(x).*cos(y/sqrt(2)))+1; % Griewank
+
+xl = [-5; -5];
+xu = [5; 5];
+
+G = 150;
+N = 50;
+D = 2;
+
+F = 0.6;
+CR = 0.9;
+
+x = zeros(D,N);
+fitness = zeros(1,N);
+
+for i=1:N
+   x(:,i) = xl+(xu-xl).*rand(D,1);
+   fitness(i) = f(x(1,i),x(2,i));
+end
+
+fx_plot = zeros(1,G);
+
+for n=1:G
+     Plot_Contour(f,x,xl,xu); % Gráfica
+    
+    for i=1:N
+        % Mutación
+        r1 = i;
+        while r1==i
+            r1 = randi([1,N]);  
+        end
+
+        r2 = r1;
+        while r2==r1 || r2==i
+            r2 = randi([1,N]);
+        end
+
+        r3 = r2;
+        while r3==r2 || r3==r1 || r3==i
+            r3 = randi([1,N]);
+        end
+        
+        v = x(:,i) + F*(x(:,r1)-x(:,i)) + F*(x(:,r2)-x(:,r3));
+
+        % Recombinación
+        u = x(:,i);	% vector de prueba
+        j = randi([1,D]);   % con randi entre {1,D}
+        L = 1;
+
+        
+        while rand()<=CR && L<=D
+            u(j) = v(j);
+            j = 1 + mod(j,D);
+            L = L + 1;
+        end
+
+        % Selección
+        fitness_u = f(u(1),u(2));
+        
+        if fitness_u < fitness(i)
+            x(:,i) = u;
+            fitness(i) = fitness_u;
+        end
+    end
+    
+    fx_plot(n) = min(fitness);
+end
+
+[~,igb] = min(fitness);
+
+figure
+Plot_Surf(f,x,xl,xu) % Gráfica
+disp([' mínimo global en: x=' num2str(x(1,igb)) ', y=' num2str(x(2,igb)) ', f(x,y)=' num2str(fitness(igb))])
+
+figure
+grid on
+hold on
+plot(fx_plot,'LineWidth',2)
+title('Convergencia')
+xlabel('iteraciones')
+ylabel('f(x)')
+
